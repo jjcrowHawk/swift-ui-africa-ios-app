cfdf4cfdd2d1d857a5674ddd889ba79939cf825b
jjcrow <hawkcrowxx@gmail.com>
2022-02-16 07:59:03 -0500

    feature(Video View, Map View): Implementation of al video list view with autoplay features and a MapView with custom animated annotations

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct VideoListView: View {
+    
+    // MARK: - PROPS
+    
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    NavigationLink(destination: VideoPlayerView(videoSelected: item.id, videoTitle: item.name)) {
+                        VideoListItemView(video: item)
+                            .padding(.vertical, 8)
+                    }
+                }//: LOOP
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button(action:{
+                           videos.shuffle()
+                           hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        }//: NAVIGATION
     }
 }
 
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
+            .previewDevice("iPhone 11 Pro")
     }
 }
---
+++ Africa/View/VideoListItemView.swift
@@ -0,0 +1,58 @@
+//
+//  VideoListItem.swift
+//  Africa
+//
+//  Created by Macbook Pro on 2/15/22.
+//
+
+import SwiftUI
+
+struct VideoListItemView: View {
+    
+    // MARK: - PROPS
+    
+    let video: Video
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack {
+            ZStack {
+                Image(video.thumbnail)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .cornerRadius(9)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 4 )
+            }//: ZSTACK
+            
+            VStack(alignment: .leading, spacing: 10){
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+            }
+        }//: HSTACK
+    }
+}
+
+struct VideoListItem_Previews: PreviewProvider {
+    
+    static let videos: [Video] = Bundle.main.decode("videos.json")
+    
+    static var previews: some View {
+        VideoListItemView(video: videos[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
